6a983217581f2adc1dd1c4a31c0fea4df5a96ce8
Ariel <ariellunen@gmail.com>
2020-12-29 18:53:16 +0200

    result page with all trips

--- Client Side/js/results.js
+++ Client Side/js/results.js
@@ -66,21 +66,25 @@ function filterCity(trip, city, dstart, dend){
 }
 
 function appendTrips(trip){
-    // $(".card-title").append("ddddd");
-    $("#All-trips").append(
-        '<p>' +
-        'ID: ' + trip.id + '<br>' +
+    var html =  '<section class="col mb-4">';
+    html += '<div class="card bg-light text-dark">';
+    html += '<div class="card-body">';
+    html += '<h5 class="card-title">'+ trip.tour_date +'</h5>';
+    html += '<p class="card-text">'+ trip.stops +'</p>';
+    html += ' <p class="card-text">' +
+        'tour ID: ' + trip.id + '<br>' +
         'tour guide: ' + trip.tour_guide + '<br>' +
         'trip name city: '  + trip.trip_name_city + '<br>' +
-        'tour date: ' + trip.tour_date + '<br>' +
         'tour time: ' + trip.tour_time + '<br>' +
         'start time: ' + trip.start_time + '<br>' +
         'spaces left: ' + trip.spaces_left + '<br>' +
-        'stops: ' + trip.stops + '<br>' +
-
         '<button class=join-trip id='+ trip.id+' >Join Trip</button>'+
-        '<br><p>'    
-    );
+        '</br></p>';
+    html += '</div>';
+    html += '</div>';
+    html += '</section>';
+
+    $("#All-trips").append(html);
     $(".join-trip").click(function(event){
         event.preventDefault();
         localStorage.setItem("trip_id", this.id);
@@ -93,10 +97,6 @@ function searchResult(){
     var city = localStorage.getItem("city");
     var dstart = localStorage.getItem("dstart");
     var dend = localStorage.getItem("dend");
-    console.log(city);
-    console.log(dstart);
-    console.log(dend);
-
     getAllTripCity(city, dstart,dend);
             
 }
